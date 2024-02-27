53114993b59e686c47000ee6c3f7a77415ba0d6f
Billiant Baer <Billdabaer@gmail.com>
2024-02-26 22:24:23 -0800

    first commit

---
+++ functions.js
@@ -0,0 +1,10 @@
+function clickAlert(){
+    alert('Your cart is empty')   
+}
+
+function hoverover(img){
+img.src= "succulents-2.jpg"
+}
+function hoverout(img){
+    img.src= "succulents-1.jpg"
+}
